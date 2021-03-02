db033a785c506775b75d78d493c261cd731a7629
Chloe Adcock <adcockchloe@gmail.com>
2021-03-02 10:04:29 +0000

    Imported login view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,8 @@
+from rest_framework_jwt.views import obtain_jwt_token
 from django.urls import path
 from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
+    path('login/', obtain_jwt_token),
 ]
