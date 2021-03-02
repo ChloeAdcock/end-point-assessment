182e3f940730b1629ca7fc2f900f59f1175972bd
Chloe Adcock <adcockchloe@gmail.com>
2021-03-02 10:48:25 +0000

    Event views

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from events.models import Event
+from rest_framework import permissions, generics
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventCreateView(generics.CreateAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+class EventListview(generics.ListAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    queryset = Event.objects.all()
