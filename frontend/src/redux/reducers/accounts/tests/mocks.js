d7574fd17d23f5e87bcd791c823b0e71af6efeff
Chloe Adcock <adcockchloe@gmail.com>
2021-03-03 11:23:20 +0000

    Tested get user reducer

--- frontend/src/redux/reducers/accounts/tests/mocks.js
+++ frontend/src/redux/reducers/accounts/tests/mocks.js
@@ -3,6 +3,8 @@ import {
   LOGIN_FAILURE,
   REGISTER_FAILURE,
   REGISTER_SUCCESS,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
 } from "../../../actions/types";
 
 const initialStateMock = {
@@ -10,6 +12,7 @@ const initialStateMock = {
   currentUserId: null,
   loginError: null,
   registerError: null,
+  getUserError: null,
 };
 
 const loginSuccessActionMock = {
@@ -30,6 +33,14 @@ const registerSuccessActionMock = {
   },
 };
 
+const getUserSuccessActionMock = {
+  type: GET_USER_SUCCESS,
+  payload: {
+    username: "test",
+    id: 1,
+  },
+};
+
 const loginSuccessMock = {
   currentUser: "test",
   currentUserId: 1,
@@ -42,12 +53,22 @@ const registerSuccessMock = {
   registerError: false,
 };
 
+const getUserSuccessMock = {
+  currentUser: "test",
+  currentUserId: 1,
+  getUserError: false,
+};
+
 const loginErrorActionMock = {
   type: LOGIN_FAILURE,
 };
 
 const registerErrorActionMock = {
   type: REGISTER_FAILURE,
+};
+
+const getUserErrorActionMock = {
+  type: GET_USER_FAILURE,
 };
 
 const loginErrorMock = {
@@ -58,16 +79,24 @@ const registerErrorMock = {
   registerError: true,
 };
 
+const getUserErrorMock = {
+  getUserError: true,
+};
+
 const mocks = {
   initialState: initialStateMock,
   registerSuccess: registerSuccessMock,
   loginSuccess: loginSuccessMock,
+  getUserSuccess: getUserSuccessMock,
   registerError: registerErrorMock,
   loginError: loginErrorMock,
+  getUserError: getUserErrorMock,
   registerSuccessAction: registerSuccessActionMock,
   loginSuccessAction: loginSuccessActionMock,
+  getUserSuccessAction: getUserSuccessActionMock,
   registerErrorAction: registerErrorActionMock,
   loginErrorAction: loginErrorActionMock,
+  getUserErrorAction: getUserErrorActionMock,
 };
 
 export default mocks;
