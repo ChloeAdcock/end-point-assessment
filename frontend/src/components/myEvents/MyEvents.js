9d627a3ac5bb7539e696424d66df6fe8110c4aff
Chloe Adcock <adcockchloe@gmail.com>
2021-03-04 16:38:29 +0000

    Drawer contect below appbar

--- frontend/src/components/myEvents/MyEvents.js
+++ frontend/src/components/myEvents/MyEvents.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Button from "@material-ui/core/Button";
+import formatDateTime from "../../helpers/formatDateTime";
 
-function MyEvents () {
-    return (
-        <Drawer
-        variant="temporary"
-      >
-          <Typography>Drawer</Typography>
-      </Drawer>
-    )
+function MyEvents(props) {
 
+  return (
+    <Drawer variant="permanent">
+      <List style={{marginTop: '20%'}}>
+        {props.events.map((event) => {
+          return (
+            <ListItem id={event.id}>
+              <Typography>{event.name}</Typography>
+              <Typography>{formatDateTime(event.date_time)}</Typography>
+              <Button onClick={() => props.handleClick(event)}>Details</Button>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Drawer>
+  );
 }
 
 export default MyEvents;
