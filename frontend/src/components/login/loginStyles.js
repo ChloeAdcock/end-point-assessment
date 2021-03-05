24459022e2504a1679820c686f2159bb8e8907d8
Chloe Adcock <adcockchloe@gmail.com>
2021-03-05 09:29:57 +0000

    Styles login

---
+++ frontend/src/components/login/loginStyles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const useStyles = makeStyles({
+  container: {
+    textAlign: 'center',
+    marginTop: '5%',
+  },
+  button: {
+      marginTop: '5%',
+      marginBottom: '10%'
+  },
+  typography: {
+      fontSize: 25,
+  }
+});
